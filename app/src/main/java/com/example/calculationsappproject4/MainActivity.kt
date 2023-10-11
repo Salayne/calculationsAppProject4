c6c8f32446d8a502047989d2e84534411ed5cfa0
@Salayne <salaynedowell@gmail.com>
2023-10-11 00:34:18 -0400

    final iteration

--- app/src/main/java/com/example/calculationsappproject4/MainActivity.kt
+++ app/src/main/java/com/example/calculationsappproject4/MainActivity.kt
@@ -4,31 +4,49 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        calculate()
+    }
 
-        val inputNum: EditText = findViewById(R.id.numericInput)
+    private fun calculate() {
+        val inputNumText: EditText = findViewById(R.id.numericInput)
         val kgButton: Button = findViewById(R.id.kgButton)
         val lbsButton: Button = findViewById(R.id.lbsButton)
-        val result = " "
+        val clearButton: Button = findViewById(R.id.clearButtton)
+        val display = findViewById<TextView>(R.id.resultsDisplay)
 
 
 
-        kgButton.setOnClickListener { }
-        lbsButton.setOnClickListener { }
-        inputNum.setOnClickListener {
-            //If kgButton clicked
-            //then  result = inputNum * 2.205
-            //If lbsButton clicked
-            //then result = inputNum / 2.205
-            //print result
+
+        kgButton.setOnClickListener {
+            val inputValue = inputNumText.text.toString().toDouble()
+            val result = inputValue * 2.2
+            //2 decimal place format
+            val formattedResult = "%.2f".format(result)
+
+            display.text = "$formattedResult"
+        }
+
+        lbsButton.setOnClickListener {
+            val inputValue = inputNumText.text.toString().toDouble()
+            val result = inputValue / 2.2
+            //2 decimal place format
+            val formattedResult = "%.2f".format(result)
+
+            display.text = "$formattedResult"
         }
 
 
+        clearButton.setOnClickListener {
+            display.text = " "
+            inputNumText.text.clear()
+        }
 
-
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
+
 }
